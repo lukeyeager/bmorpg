f8596b8efc31dcad8b0ffbca710a0ac5a38cf296
Luke Yeager <luke.yeager@gmail.com>
2012-02-06 23:00:21 +0000

    Whoops, added wrong project. Here is the real initial commit.

---
+++ Server/Server.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace BMORPG_Server
+{
+    // Source: http://msdn.microsoft.com/en-us/library/5w7b7x5f.aspx
+    // State object for reading client data asynchronously.
+    public class StateObject
+    {
+        // Client  socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BufferSize = 1024;
+        // Receive buffer.
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    class Server
+    {
+        // Thread signal.
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        // Source: http://msdn.microsoft.com/en-us/library/5w7b7x5f.aspx
+        // Listens for clients to connect.
+        public static void Start()
+        {
+            // prompt user for port number (This is here to test local-vs-remote-AI using only this server meaning two different port numbers are needed)
+            Console.WriteLine("Port Number:");
+            int port = Convert.ToInt32(Console.ReadLine());
+            // Establish the local endpoint for the socket.
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.
+                    allDone.Reset();
+
+                    // Start an asynchronous socket to listen for connections.
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+
+                    // Wait until a connection is made before continuing.
+                    allDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        // Source: http://msdn.microsoft.com/en-us/library/5w7b7x5f.aspx
+        // Welcomes a new client and listens.
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.
+            allDone.Set();
+
+            // Get the socket that handles the client request.
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            // WELCOME
+            Console.WriteLine("Welcoming new client...");
+            Send(handler, "WELCOME\n\r");
+
+            // Create the state object.
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+
+            // Receive game type from client.
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+        }
+
+        // Source: http://msdn.microsoft.com/en-us/library/5w7b7x5f.aspx
+        // Sends a string from the server to a client using the given socket.
+        private static void Send(Socket handler, String data)
+        {
+            Console.WriteLine("SENT:\t{0}", data);
+            // Convert the string data to byte data using ASCII encoding.
+            Byte[] byteData = System.Text.Encoding.ASCII.GetBytes(data);
+
+            try
+            {
+                // Begin sending the data to the remote device.
+                handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Source: http://msdn.microsoft.com/en-us/library/5w7b7x5f.aspx
+        // Specifies what was sent to the client by the server.
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Retrieve the socket from the state object.
+                Socket handler = (Socket)ar.AsyncState;
+
+                if (handler.Connected)
+                {
+                    // Complete sending the data to the remote device.
+                    int bytesSent = handler.EndSend(ar);
+                    //Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+                }
+                else
+                {
+                    Console.WriteLine("Unable to send. Player left.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        // Format Source: http://msdn.microsoft.com/en-us/library/5w7b7x5f.aspx
+        // Receive game type from client.
+        public static void ReadCallback(IAsyncResult ar)
+        {
+            String content = String.Empty;
+
+            // Retrieve the state object and the handler socket from the asynchronous state object.
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            try
+            {
+                if (handler.Connected)
+                {
+                    // Read data from the client socket. 
+                    int bytesRead = handler.EndReceive(ar);
+                    //Console.WriteLine("\tBytes Read = {0}", bytesRead);
+
+                    if (bytesRead > 0)
+                    {
+                        // data.ToString()
+                        state.sb.Clear();
+                        state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                        content = state.sb.ToString();
+
+                        // ignore newline and the initial read
+                        if (content == "\r\n" || content == "???? ????'??????")
+                        {
+                            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadCallback), state);
+                            return;
+                        }
+
+                        Console.WriteLine("READ:\t{0}", content);
+
+                        // Decide what to do with the input here...
+
+                    }
+                    else
+                    {
+                        // ILLEGAL
+                        Send(handler, "ILLEGAL\n\r");
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
