2e06155206032db55bf8ece597e8697940607a84
Josue Martinez <josue.mrtnz@gmail.com>
2012-02-08 22:56:17 +0000

    Adds networking

--- Client/RpgFrontEnd/RPGProofOfConcept/MainWindow.xaml.cs
+++ Client/RpgFrontEnd/RPGProofOfConcept/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,11 +19,37 @@ namespace RPGProofOfConcept
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+
+        private IPAddress ipAddress;
+        private IPEndPoint ipEndpoint;
+        private TcpClient client;
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
-
-			// Insert code required on object creation below this point.
+            client = new TcpClient();
+            connectToServer();
 		}
+
+        public void connectToServer()
+        {
+            ipAddress = IPAddress.Parse("127.0.0.1");
+            ipEndpoint = new IPEndPoint(ipAddress, Int32.Parse("11000"));
+            client.Connect(ipEndpoint);
+            NetworkStream stream = client.GetStream();
+            Byte[] data = new Byte[1024];
+            String responseData = String.Empty;
+
+            try
+            {
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                MessageBox.Show(responseData);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
 	}
 }
