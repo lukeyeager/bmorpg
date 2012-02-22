c49a134f04f34290f6fd85a6597d3f4a5fb3a29b
Luke Yeager <luke.yeager@gmail.com>
2012-02-22 17:20:08 +0000

    Updated the Batch server to use either Debug or Release executable, whichever exists (Checks Release first).
    Also removed obj files from the SVN. Please don't add these, we don't need to keep them in revision history.

--- RestartServer.bat
+++ RestartServer.bat
@@ -1,13 +1,41 @@
-REM	RestartServer.bat
-REM	Feb 21, 2012
-REM	TAMU, CSCE 431
-REM	Luke Yeager
+echo off
 
-REM	Update from SVN
+REM		RestartServer.bat
+REM		Feb 21, 2012
+REM		TAMU, CSCE 431
+REM		Luke Yeager
+
+SET		serverDir=Server\bin
+SET		releaseDir=%CD%\%serverDir%\Release
+SET		debugDir=%CD%\%serverDir%\Debug
+SET		serverExe=BMORPG_SERVER.exe
+SET		serverPort=10000
+
+echo	Updating the SVN...
 TortoiseProc /command:update /path:"%CD%" /closeonend:1
 
-REM	Restart the server
-CD	"Server\bin\Release"
-START	/B BMORPG_SERVER.exe 10000
+echo	Restarting the server...
+
+REM		NOTE: This script uses the Release version of the executable if it can
+IF EXIST	%releaseDir%\%serverExe% (
+	CD		%releaseDir%
+	FOR %%? IN (%serverExe%) DO (
+		ECHO	Using executable: %releaseDir%\%serverExe%
+		ECHO	Last modified: %%~t?
+	)
+	START	/B  %serverExe% %serverPort%
+) ELSE (
+	REM		If the release executable is not available, it uses the Debug executable
+	IF EXIST	%debugDir%\%serverExe% (
+		CD		%debugDir%
+		FOR %%? IN (%serverExe%) DO (
+			ECHO	Using executable: %debugDir%\%serverExe%
+			ECHO	Last modified: %%~t?
+		)
+		START	/B %serverExe% %serverPort%
+	) ELSE (
+		ECHO	ERROR: Could not find %serverExe%!
+	)
+)
 
 EXIT
