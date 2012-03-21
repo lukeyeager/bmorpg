5404a2fc9af60a7a724e1cfcd550232604edd7a7
Brandon Adame <>
2012-03-21 22:13:52 +0000

    Added PlayerMovePacket.cs to be used in Game threads. Updated NetworkPacket deserialize function to include the new packet type. Added notes to Game.cs

---
+++ Shared/NetworkPackets/PlayerMovePacket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BMORPG.NetworkPackets
+{
+    [Serializable]
+    class PlayerMovePacket : NetworkPacket
+    {
+        [NonSerialized]
+        public const String Identifier = "PLAYER_MOVE";
+
+        // TODO:
+        // Add more information needed for player X to make a move
+
+        public PlayerMovePacket()
+        {
+            PacketType = Identifier;
+        }
+
+    }
+}
